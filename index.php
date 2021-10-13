adb77928925879aad00fcba8f6e5730c79ceecba
Lucas Magno <50931408+lucasmag26@users.noreply.github.com>
2021-10-13 20:45:26 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html >
+<meta charset="utf-8"> 
+<!-- Bootstrap CSS -->
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!--BOOTSTRAP JAVASCRIPT-->  
+<script src="bootstrap/js/bootstrap.min.js"></script> 
+
+<html>
+    <head>
+        <title>Santa Casa </title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="#"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="formularios/paciente.php">Cadastrar Paciente <span class="sr-only"></span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="formularios/internacao.php">Cadastrar Internação  <span class="sr-only"></span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="formularios/exibeInternacao.php">Consulta Internação  <span class="sr-only"></span></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        
+    </body>
+</html>
